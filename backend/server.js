7790250f3c38d2ba43fdccf9092edb946087d0f5
LukeHoward4 <124386387+LukeHoward4@users.noreply.github.com>
2024-12-06 18:14:55 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = process.env.port || 3001;
 const mysql= require("mysql2")
-
+const cors = require("cors")
 const pool = mysql.createPool({
   host:"localhost",
   user:"root",
@@ -11,10 +10,24 @@ const pool = mysql.createPool({
   database: "finalprojectdatabase",
   connectionLimit:10
 })
+app.use(cors())
 
+const corsOptions = {
+    origin: "http://localhost:3000",
+    allowHeaders: ["Content-type", "Authorization"]
+};
 pool.query('select * from finalprojectdatabase.account', ( err, res)=>{
   return console.log(res)
 })
-
-app.get('/', (req, res) => res.send("hello world"))
-app.listen(port, () => console.log("app running on port 3000"))
+const foods = [
+    { id: 1, name: 'Banana', type: 'fruit', calories: 50 },
+    { id: 2, name: 'Apple', type: 'fruit', calories: 75 },
+    { id: 3, name: 'Sourdough', type: 'bread', calories: 100 },
+    { id: 4, name: 'Beans', type: 'protein', calories: 100 },
+    { id: 5, name: 'Steak', type: 'protein', calories: 200 },
+    { id: 6, name: 'Broccoli', type: 'vegetable', calories: 50 },
+  ];
+app.get('/api/foods', (req, res) => {
+    res.json(foods);
+  });
+app.listen(port, () => console.log("app running on port " + port))
